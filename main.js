486cf918e23a10185b60d3b7ee7b6e5939a6e22e
GIGIPR3 <gigiprencipe47@gmail.com>
2022-11-11 10:53:47 +0100

    .

---
+++ main.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        titolo: 'giulia',
+        immagine: 'https://cdn.motor1.com/images/mgl/KpgwG/s1/alfa-romeo-giulia-quadrifoglio-drift.jpg',
+    },
+    methods: {
+        /* genera: function(){
+            if (this.titolo == 'giulia'){
+                document.getElementById('ciao').innerHTML = `<img  v-bind:src="immagine" alt="" id="foto"></img>`;
+            } else {
+                console.log(titolo); 
+            }
+        } */
+    }
+
+});
